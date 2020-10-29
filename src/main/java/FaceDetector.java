c118a765ad1c911888371f1e5cb08618dfc1d00a
Tzesh <mrtzesh@gmail.com>
2020-10-29 13:09:19 +0300

    Initial commit

---
+++ src/main/java/FaceDetector.java
@@ -0,0 +1,26 @@
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+public class FaceDetector {
+    public static void detectAndDisplay(String pathName) {
+        nu.pattern.OpenCV.loadLocally();
+
+        String imgFile = "images/ellenselfie-1280x720.png";
+        Mat src = Imgcodecs.imread(imgFile);
+
+        String xmlFile = "xml/lbpcascade_frontalface.xml";
+        CascadeClassifier cc = new CascadeClassifier(xmlFile);
+
+        MatOfRect faceDetection = new MatOfRect();
+        cc.detectMultiScale(src, faceDetection);
+        System.out.println(String.format("Detected faces: %d", faceDetection.toArray().length));
+
+        for (Rect rect : faceDetection.toArray())
+            Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255), 3);
+
+        Imgcodecs.imwrite("images/messivsronaldo_out.png", src);
+        System.out.println("Image Detection Finished");
+    }
+}
